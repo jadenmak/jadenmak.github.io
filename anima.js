d95a90688a67d7f0251a06b5104b4a8a6f0f8c6c
Jaden Mak <41302497+jadenmak@users.noreply.github.com>
2018-07-20 14:02:22 -0400

    Update anima.js

--- anima.js
+++ anima.js
@@ -1,1 +1,8 @@
 document.getElementById('demo').innerHTML='Hello Javascript'
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
